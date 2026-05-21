f2cda506e2f85c4097a3cdc0da7566e20ed29e09
ChenYi Agent <chenyi@example.com>
2026-05-22 01:41:57 +0800

    fix: 添加 Log 导入

--- android/app/src/main/java/com/chenyi/agent/MainActivity.kt
+++ android/app/src/main/java/com/chenyi/agent/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
